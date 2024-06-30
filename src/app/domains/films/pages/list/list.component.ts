2fab2161e779921596a963872243974fc925fa96
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-06-30 17:19:28 -0500

    ci: apply update in list view

--- src/app/domains/films/pages/list/list.component.ts
+++ src/app/domains/films/pages/list/list.component.ts
@@ -1,33 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FilmService } from './../../services/film.service';
 import { Film } from './../../../shared/models/film.model';
-import { FilmComponent } from './../../components/film/film.component';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  filmEdited?: any;
 
   filmList: Film[] = [];
-  constructor(private readonly filmService: FilmService) { }
+  constructor(
+    private readonly filmService: FilmService,
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.getFilmList();
+
+    console.log('la pelicula editada llego a la lista: ', history.state.film);
   }
 
   getFilmList() {
     this.filmService.getFilms().subscribe(
       (response) => {
         this.filmList = response.results;
-        console.log("response: ", this.filmList);
+        console.log('response: ', this.filmList);
+        this.updateFilm();
       },
       (error) => {
         console.error('Error fetching films', error);
       }
     );
   }
-  
 
+  updateFilm() {
+    this.filmEdited = history.state.film;
+    this.filmList = this.filmList.map((film) => {
+      console.log("id: ", film.id, this.filmEdited?.id);
+      if (film.id === this.filmEdited?.id) {
+        console.log("entro", this.filmEdited?.title);
+        return {
+          ...film,
+          title: this.filmEdited?.title,
+          vote_average: this.filmEdited?.vote_average,
+          overview: this.filmEdited?.overview,
+        };
+      }
+      return film;
+    });
+    this.cdRef.detectChanges();
+    console.log('este es mi list final: ', this.filmList);
+  }
 }
--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() film?: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToDetail(film: any): void {
+    this.router.navigate(['/'], { state: { film } });
+    console.log("el film del url", this.film);
   }
 
 }
